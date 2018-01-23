ee6bf0661dc5e97dbe9582fe65861ddbd2dbbd2b
Anton Hörl <anton.hoerl@gmx.at>
2018-01-23 14:53:18 +0100

    UI adapted

--- angular-src/src/app/components/digitalerTischplan/notiz/notiz.component.ts
+++ angular-src/src/app/components/digitalerTischplan/notiz/notiz.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-notiz',
   templateUrl: 'notiz.component.html',
-  styleUrls: ['notiz.component.css']
+  styleUrls: ['../tischplan.component.css']
 })
 export class NotizComponent implements OnInit {
   @Input('notizElements') notizElements: any;
